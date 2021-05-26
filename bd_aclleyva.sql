b19bb08c9ed23b6b112ed90a3257b30a2729380c
SNLEYVA <80928668+SNLEYVA@users.noreply.github.com>
2021-05-25 23:38:27 -0400

    Add files via upload

---
+++ bd_aclleyva.sql
@@ -0,0 +1,61 @@
+﻿# Host: localhost:3307  (Version 5.5.5-10.1.35-MariaDB)
+# Date: 2021-05-25 23:37:41
+# Generator: MySQL-Front 6.1  (Build 1.21)
+
+
+#
+# Structure for table "roles"
+#
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "roles"
+#
+
+INSERT INTO `roles` VALUES (3,'Capitan de Ejercito'),(5,'Administrador');
+
+#
+# Structure for table "usuarios"
+#
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(20) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `clave` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "usuarios"
+#
+
+INSERT INTO `usuarios` VALUES (8,'albertxxxLEYVAxxx','albert@gmail.com','c37bf859faf392800d739a41fe5af151');
+
+#
+# Structure for table "permisos"
+#
+
+DROP TABLE IF EXISTS `permisos`;
+CREATE TABLE `permisos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_usuario` (`id_usuario`),
+  KEY `fk_rol` (`id_rol`),
+  CONSTRAINT `fk_rol` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`),
+  CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "permisos"
+#
+
